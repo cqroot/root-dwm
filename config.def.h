5bdcd589e6ea3f5c7b141f0b65bf13df433209f5
cqroot <cqroot@outlook.com>
2022-08-09 13:23:02 +0800

    custom: modify the default terminal and font

--- config.def.h
+++ config.def.h
@@ -11,8 +11,8 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
-static const char *fonts[]          = { "CaskaydiaCove Nerd Font:size=10" };
-static const char dmenufont[]       = "CaskaydiaCove Nerd Font:size=10";
+static const char *fonts[]          = { "CaskaydiaCove Nerd Font:size=12", "WenQuanYi Micro Hei:size=12" };
+static const char dmenufont[]       = "CaskaydiaCove Nerd Font:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -64,7 +64,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
