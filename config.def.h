a21623e539bf04400943b5b687c5c8204b0e00b6
cqroot <cqroot@outlook.com>
2022-08-10 14:06:11 +0800

    custom: use <C-s> to open dmenu

--- config.def.h
+++ config.def.h
@@ -74,6 +74,7 @@ static const char *voldowncmd[] = { "pulseaudio-ctl", "down", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
